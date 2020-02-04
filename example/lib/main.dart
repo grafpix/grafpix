7aef43c56d1f13668786fc0385250ed0aab44a7f
Ahmed Mustafa <59063329+grafpix@users.noreply.github.com>
2020-02-04 23:55:24 +0200

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,34 +1,38 @@
 // Example
 
-import 'package:grafpix/pixloaders/pix_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:grafpix/icons.dart';
+import 'package:grafpix/pixloaders/pix_loader.dart';
+import 'package:grafpix/pixbuttons/radial.dart';
 
 class Example extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[600],
       body: FutureBuilder(
           future: waitForFuture(),
           builder: (context, snapshot) {
             if (snapshot.hasData)
+              // display PixButton with PixIcon when Future ends.
               return Container(
                 alignment: Alignment.center,
-                child: IconButton(
-
-                    // display PixIcon when Future ends.
-                    icon: Icon(PixIcon.pix_android,
-                        size: 30.0, color: Colors.green[600]),
-                    onPressed: () {
+                child: PixButton(
+                    radius: 70.0,
+                    icon: PixIcon.pix_android, //PixIcon
+                    iconSize: 50.0,
+                    iconColor: Colors.white,
+                    backgroundColor: Colors.blue,
+                    shutter: 0.6,
+                    twinkles: true,
+                    onPress: () {
                       print('Icon Pressed');
                     }),
               );
             else
-
               // display PixLoader while Future in progress.
               return PixLoader(
-                  loaderType: LoaderType.Spinner,
-                  faceColor: Colors.pinkAccent[400]);
+                  loaderType: LoaderType.Flashing, faceColor: Colors.white70);
           }),
     );
   }
